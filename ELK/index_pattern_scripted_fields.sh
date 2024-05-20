26e725463f8e95948c678e4446c6be013bef7baa
kenny <kennywong@ghero.tech>
2024-05-20 17:19:10 +0800

    scripted fields

---
+++ ELK/index_pattern_scripted_fields.sh
@@ -0,0 +1,11 @@
+#this will display api called without the variables passed, after '?' are the var
+if (doc['request.keyword'].size() > 0) {
+    def url = doc['request.keyword'].value;
+    def index = url.indexOf('?');
+    if (index > 0) {
+        return url.substring(0, index);
+    } else {
+        return url;
+    }
+}
+return "";
