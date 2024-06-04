57a1656ba2fdaa02b818240b2314707ac5c1acbe
kenny <154249667+kennywo-ng@users.noreply.github.com>
2024-06-04 17:59:09 +0800

    Update index_pattern_scripted_fields.sh

--- ELK/index_pattern_scripted_fields.sh
+++ ELK/index_pattern_scripted_fields.sh
@@ -1,4 +1,5 @@
 #this will display api called without the variables passed, after '?' are the var
+#this is configured on kibana index pattern
 if (doc['request.keyword'].size() > 0) {
     def url = doc['request.keyword'].value;
     def index = url.indexOf('?');
